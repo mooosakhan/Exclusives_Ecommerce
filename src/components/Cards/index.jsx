438d69bb83c3b423777b039344e1bb64e2aaba0e
Moosa <23-AI-13@students.duet.edu.pk>
2024-12-09 15:21:28 +0000

    k

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
-    <div>
-      
+    <div className="mx-[9vw] flex justify-center items-center">
+      <div className="card1  bg-zinc-700 h-60 w-30">
+        vcxb
+        </div> 
+      <div className="card2 bg-zinc-700 h-60 w-30"></div>
+      <div className="card3 bg-zinc-700 h-60 w-30"></div> 
+      <div className="card4 bg-zinc-700 h-60 w-30"></div>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
